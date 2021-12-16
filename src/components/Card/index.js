fd76196454aab790498b247e3517c1ce80913653
andrea.guzman <andrea.guzman@payclip.com>
2021-12-16 14:12:38 -0600

    added height:230 image

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -42,7 +42,7 @@ const PokemonCard = (props) => {
     <Card sx={{ Width: 345 }}>
       <CardMedia
         component="img"
-        height="200"
+        height="230"
         sx={{ alignSelf:"center" }}
         image={image}
         alt="pokemon"
